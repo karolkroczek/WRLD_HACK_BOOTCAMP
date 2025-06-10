4ade3af659e19b7bdc47708e069184a1f45f70a1
Karol <karolkroczek10@gmail.com>
2025-06-10 19:45:12 +0200

    add translation service

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -1,3 +1,9 @@
+use ic_cdk::api::management_canister::http_request::http_request;
+use ic_cdk::api::management_canister::http_request::CanisterHttpRequestArgument;
+use ic_cdk::api::management_canister::http_request::HttpMethod;
+use ic_cdk::api::management_canister::http_request::HttpHeader;
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
@@ -17,4 +23,37 @@ fn calculate(a: i32,b:i32,operator:String) -> String {
         Some(value) => format!("Result: {}",value),
         None => "Wrong operator or division by zero".to_string()
     }
+}
+
+
+#[ic_cdk::update]
+async fn translate(text: String) -> String {
+    let token = "";
+    
+    let (res,) = http_request(
+        CanisterHttpRequestArgument {
+            url: "https://api-inference.huggingface.co/models/google-t5/t5-base".to_string(),
+            max_response_bytes: None,
+            method: HttpMethod::POST,
+            headers: vec![
+                    HttpHeader {
+                    name: "Authorization".to_string(),
+                    value: format!("Bearer {}", token),
+                },
+                    HttpHeader {
+                    name: "Content-Type".to_string(),
+                    value: "application/json".to_string(),
+                }
+            ],
+            body: Some(format!(r#"{{
+               "inputs": "{}"
+            }}"#,text).into()),
+            transform: None,
+        },20_850_122_800u128 + text.len() as u128 *5200u128,
+    ).await.unwrap();
+
+    let body = String::from_utf8(res.body).unwrap();
+    ic_cdk::println!("{:?}",body);
+
+    format!("{}", body)
 }
